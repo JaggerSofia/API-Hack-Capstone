9863afbbf3088cb0bb339083a89f64347f449945
Victoria Moore <victoriamoore563@gmail.com>
2019-10-12 14:52:16 -0700

    a

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-const hikingApiKey = '200616064-e1179b9f85f40484fd23132602b1b789';
+const hikingKey = '200616064-e1179b9f85f40484fd23132602b1b789';
 const weatherKey = '273aa1cbe2cd87586632673b6637c0de';
 const mapKey = 'AIzaSyDog9d8kc7VcB88_lhRhJvXdookxEfLrCs';
+
