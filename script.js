fa06321bf777306fc5c3ce121536f670acb3fa32
Victoria Moore <victoriamoore563@gmail.com>
2019-10-15 19:32:45 -0700

    added all keys & URLs

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-// const hikingKey = '200616064-e1179b9f85f40484fd23132602b1b789';
-// const weatherKey = '273aa1cbe2cd87586632673b6637c0de';
-// const mapKey = 'AIzaSyDog9d8kc7VcB88_lhRhJvXdookxEfLrCs';
+const hikingKey = '200616064-e1179b9f85f40484fd23132602b1b789';
+const hikingUrl = 'https://www.hikingproject.com/data/get-trails?lat=44.4280&lon=-110.5885&key=200616064-e1179b9f85f40484fd23132602b1b789';
+
+const weatherKey = '273aa1cbe2cd87586632673b6637c0de';
+const weatherUrl ='api.openweathermap.org/data/2.5/forecast?lat=44.4280&lon=-110.5885&APIKey=273aa1cbe2cd87586632673b6637c0de';
+
+const mapKey = 'AIzaSyDog9d8kc7VcB88_lhRhJvXdookxEfLrCs';
+const mapUrl='https://maps.googleapis.com/maps/api/geocode/json?address=Yellow+Stone+National+Park&key=AIzaSyDog9d8kc7VcB88_lhRhJvXdookxEfLrCs&latlng=44.427963,-110.588455';
+
 
