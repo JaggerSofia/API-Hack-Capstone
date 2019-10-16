e0c51b1b898f604893e3a4f4a177f4013c04a4cc
Victoria Moore <victoriamoore563@gmail.com>
2019-10-15 20:22:06 -0700

    commit

--- script.js
+++ script.js
@@ -7,4 +7,44 @@ const weatherUrl ='api.openweathermap.org/data/2.5/forecast?lat=44.4280&lon=-110
 const mapKey = 'AIzaSyDog9d8kc7VcB88_lhRhJvXdookxEfLrCs';
 const mapUrl='https://maps.googleapis.com/maps/api/geocode/json?address=Yellow+Stone+National+Park&key=AIzaSyDog9d8kc7VcB88_lhRhJvXdookxEfLrCs&latlng=44.427963,-110.588455';
 
+function searchResults(responseJson) {
+    console.log(responseJson);
+    for (let i=0; i<responseJson.items.length; i++) {
+        $('#trail-list').append(
+            `<li>
+                <h3>${responseJson.items[i].lat.lng}</h3>
+                <p>${responseJson.items[i].long_name.location_type}</p>
+            </li>`
+        )
+    }
+    $('.results').removeClass('hidden')
+}
 
+function findTrails(searchTrail) {
+    const params = {
+        address = searchTrail,
+        key= mapKey,
+    };
+
+    let queryString = searchTrail = $.param(params);
+    console.log("QueryString", queryString);
+    const  url = mapUrl + '?' + queryString;
+    console.log(url);
+
+    fetch(url)
+    .then(response) => {
+        if (response.ok) {
+            return response.json();
+        }
+    .then(responseJson => searchResults(responseJson))
+    });
+}
+
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        let  = ${'#trail-local'}.val();
+
+        if 
+    })
+}
