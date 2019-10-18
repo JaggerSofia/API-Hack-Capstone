8fec58d5e3b66c532b4614e1453153084ff8487b
Victoria Moore <victoriamoore563@gmail.com>
2019-10-17 19:11:50 -0700

    js map, and trail search functioning, weather not

--- script.js
+++ script.js
@@ -1,50 +1,134 @@
-const hikingKey = '200616064-e1179b9f85f40484fd23132602b1b789';
-const hikingUrl = 'https://www.hikingproject.com/data/get-trails?lat=44.4280&lon=-110.5885&key=200616064-e1179b9f85f40484fd23132602b1b789';
+weatherKey = '273aa1cbe2cd87586632673b6637c0de';
+weatherUrl ='api.openweathermap.org/data/2.5/forecast?'
+// weatherKey = "882c100e357f5ad81cfdce3b65036425";
+hikingKey = "200616177-251562ab104a6def31b38b058d4acf3a";
+mapsKey = "AIzaSyB4NifjFp63z2lo8oXCaggg5Yrme4z5b_A";
+mapsUrl = "https://maps.googleapis.com/maps/api/geocode/json"
+// weatherUrl = "api.openweathermap.org/data/2.5/forecast?"
+hikingUrl = "https://www.hikingproject.com/data/get-trails"
 
-const weatherKey = '273aa1cbe2cd87586632673b6637c0de';
-const weatherUrl ='api.openweathermap.org/data/2.5/forecast?lat=44.4280&lon=-110.5885&APIKey=273aa1cbe2cd87586632673b6637c0de';
 
-const mapKey = 'AIzaSyDog9d8kc7VcB88_lhRhJvXdookxEfLrCs';
-const mapUrl='https://maps.googleapis.com/maps/api/geocode/json?address=Yellow+Stone+National+Park&key=AIzaSyDog9d8kc7VcB88_lhRhJvXdookxEfLrCs&latlng=44.427963,-110.588455';
 
-function searchResults(responseJson) {
-    console.log(responseJson);
-    for (let i=0; i<responseJson.items.length; i++) {
-        $('#trail-list').append(
+
+function displayResults(trailsJson) {
+    $("#results-list").empty();
+
+    console.log("displaying", trailsJson);
+
+    for(let i=0; i<trailsJson.trails.length; i++) {
+        $("#results-list").append(
             `<li>
-                <h3>${responseJson.items[i].lat.lng}</h3>
-                <p>${responseJson.items[i].long_name.location_type}</p>
+            <h3>${trailsJson.trails[i].name}</h3>
+            <img src="${trailsJson.trails[i].imgSmallMed}">
+            <p>${trailsJson.trails[i].summary}</p>
             </li>`
         )
     }
-    $('.results').removeClass('hidden')
+    $("#results").removeClass("hidden");
 }
 
-function findTrails(searchTrail) {
-    const params = {
-        address = searchTrail,
-        key= mapKey,
+function displayWeather(forcastJson) {
+    $('#weather-list').empty();
+
+    console.log('displaying', forcastJson);
+
+    for (let i=0; i<forcastJson.forcast.length; i++) {
+        $('#weather-list').append(
+            `<li>
+            <p>${forcastJson.forcast[i].main}</p>
+            <p>${forcastJson.forcast[i].weather}</p>
+            </li>`
+        )
+    }
+}
+
+function getTrails(mapsJson) {
+    console.log("maps data", mapsJson);
+
+    const params= {
+        lat: `${mapsJson.results[0].geometry.location.lat}`,
+        lon: `${mapsJson.results[0].geometry.location.lng}`,
+        key: hikingKey,
     };
 
-    let queryString = searchTrail = $.param(params);
-    console.log("QueryString", queryString);
-    const  url = mapUrl + '?' + queryString;
+    console.log(`${mapsJson.results[0].geometry.location.lat}`);
+
+    let queryString = $.param(params);
+    console.log(queryString);
+    const url = hikingUrl + "?" + queryString;
     console.log(url);
 
     fetch(url)
-    .then(response) => {
-        if (response.ok) {
-            return response.json();
-        }
-    .then(responseJson => searchResults(responseJson))
-    });
+        .then(response => {
+            if(response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        }).then(trailsJson=>displayResults(trailsJson))
+        .catch(err => {
+            $("#js-error-message").text(`Something failed: ${err.message}`);
+        });
+}
+
+function getWeather(weatherJson) {
+    const params = {
+        lat: `${mapsJson.results[0].geometry.location.lat}`,
+        lon: `${mapsJson.results[0].geometry.location.lng}`,
+    }
+    
+    let queryString = $.param(params);
+    console.log(queryString);
+    const url = weatherUrl + '?' + queryString;
+
+    fetch(url)
+        .then(response => {
+            if(response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        }).then(weatherJson=>displayResults(weatherJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something failed: ${err.message}`);
+        })
+
+}
+
+function getLocation(searchTerm) {
+    console.log("Formdata", searchTerm);
+    const params = {
+        address: searchTerm,
+        key: mapsKey,
+    };
+
+    let queryString = $.param(params);
+    console.log(queryString);
+    const url = mapsUrl + "?" + queryString;
+
+    fetch(url)
+        .then(response=> {
+            if(response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        }).then(mapsJson=>getTrails(mapsJson))
+        .catch(err => {
+            $("#js-error-message").text(`Something failed: ${err.message}`);
+        });
 }
 
 function watchForm() {
     $('form').submit(event => {
         event.preventDefault();
-        let  = ${'#trail-local'}.val();
-
-        if 
+        let searchTerm=$("#search-term").val();
+        
+        if(searchTerm != "") {
+            getLocation(searchTerm);
+        } else {
+            alert("Please enter a location");
+        }
     })
+
 }
+
+
+$(watchForm)
