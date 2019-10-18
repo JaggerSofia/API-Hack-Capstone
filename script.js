e2636aebcf3d1568fceb6ab09f6e64621b1d1c06
Victoria Moore <victoriamoore563@gmail.com>
2019-10-17 20:17:44 -0700

    classes added to add fonts

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ function displayResults(trailsJson) {
     for(let i=0; i<trailsJson.trails.length; i++) {
         $("#results-list").append(
             `<li>
-            <h3>${trailsJson.trails[i].name}</h3>
+            <h3 class='domine-font'>${trailsJson.trails[i].name}</h3>
             <img src="${trailsJson.trails[i].imgSmallMed}">
-            <p>${trailsJson.trails[i].summary}</p>
+            <p class='roboto-font'>${trailsJson.trails[i].summary}</p>
             </li>`
         )
     }
