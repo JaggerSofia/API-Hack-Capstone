25f1fcf0561b4fdd8343bfcf9dcb05079e556724
Victoria Moore <victoriamoore563@gmail.com>
2019-10-19 12:45:58 -0700

    syntax error  corrected

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ function getLocation(searchTerm) {
         .catch(err => {
             $("#js-error-message").text(`Location failed: ${err.message}`);
         });
-
+}
 
 
 // Watches submit button via event listener to get input data
@@ -114,6 +114,6 @@ function watchForm() {
             alert("Please enter a location");
         }
     })
-
 }
+
 $(watchForm)
